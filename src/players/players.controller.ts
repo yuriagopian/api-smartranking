1e3b966d75458f31a94b08bd74c05ba6ddcd7685
yuriagopian <yuri.agopian@hotmail.com>
2023-02-11 02:06:59 -0300

    chore: feat create player

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 import { PlayersService } from './players.service';
 
 @Controller('api/v1/players')
 export class PlayersController {
   constructor(private readonly playersService: PlayersService) {}
+
+  @Post()
+  createPlayer() {}
+
+  @Delete()
+  removePlayer(id) {}
+
+  @Get()
+  listPlayers() {}
+
+  @Get('id')
+  getPlayerById(id) {}
 }
