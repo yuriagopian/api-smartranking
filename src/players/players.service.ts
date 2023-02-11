e5c47a13dbef1760814cc2c8ee364030b15435fa
yuriagopian <yuri.agopian@hotmail.com>
2023-02-11 11:40:54 -0300

    chore: create player service

--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -2,13 +2,25 @@ import { Injectable, Logger } from '@nestjs/common';
 import { IPlayer } from './domain/player.interface';
 import { CreatePlayerDto } from './dtos/create-player.sto';
 
+import { v4 as uuid } from 'uuid';
+
 @Injectable()
 export class PlayersService {
   private readonly logger = new Logger(PlayersService.name);
   private players: IPlayer[] = [];
-  createPlayer(createPlayersDto: CreatePlayerDto) {
+  createPlayer(createPlayersDto: CreatePlayerDto): Promise<void> {
     this.logger.log(`create player dto: ${createPlayersDto}`);
   }
 
-  private create(createPlayersDto: CreatePlayerDto): IPlayer {}
+  private create(createPlayersDto: CreatePlayerDto): IPlayer {
+    const { email, name, phoneNumber } = createPlayersDto;
+
+    const player: IPlayer = {
+      _id: uuid(),
+      name,
+      phoneNumber,
+      email,
+    };
+    return player;
+  }
 }
