4b41f9f5435fdae26c48681e7bfd3d5c3cc90024
yuriagopian <yuri.agopian@hotmail.com>
2023-02-16 14:21:20 -0300

    chore: implements players valiation params pipe

--- src/players/pipes/players-validation-params.pipe.ts
+++ src/players/pipes/players-validation-params.pipe.ts
@@ -1,3 +1,9 @@
-import { PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
 
-export class PlayersValidationParamsPipe implements PipeTransform {}
+export class PlayersValidationParamsPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    console.log(`value: ${value} metadata: ${metadata.type}`);
+
+    return value;
+  }
+}
