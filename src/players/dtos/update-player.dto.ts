2d28d984a367819e71715663a85a91d8dc8114a5
yuriagopian <yuri.agopian@hotmail.com>
2023-02-16 15:33:43 -0300

    chore: implements players valiation params pipe

--- src/players/dtos/update-player.dto.ts
+++ src/players/dtos/update-player.dto.ts
@@ -1,5 +1,29 @@
 // import { PartialType } from '@nestjs/swagger';
 import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsString,
+  IsNotEmpty,
+  MinLength,
+  MaxLength,
+  IsEmail,
+} from 'class-validator';
 import { CreatePlayerDto } from './create-player.dto';
 
-export class UpdatePlayerDto extends PartialType(CreatePlayerDto) {}
+// export class UpdatePlayerDto extends PartialType(CreatePlayerDto) {}
+export class UpdatePlayerDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(9)
+  @MaxLength(13)
+  readonly phoneNumber: string;
+
+  @IsEmail(undefined)
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(4)
+  @MaxLength(256)
+  readonly name: string;
+}
