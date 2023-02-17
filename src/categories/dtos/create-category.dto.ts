de0057ff778e3651dc826bda6348a061b507e208
yuriagopian <yuri.agopian@hotmail.com>
2023-02-16 21:13:18 -0300

    feat:  Categories schema

--- src/categories/dtos/create-category.dto.ts
+++ src/categories/dtos/create-category.dto.ts
@@ -1,1 +1,15 @@
-export class CreateCategoryDto {}
+import { ArrayMinSize, IsArray, IsNotEmpty, IsString } from 'class-validator';
+import { CategoryEvent } from '../domains/category.interface';
+
+export class CreateCategoryDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly category: string;
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  events: Array<CategoryEvent>;
+}
