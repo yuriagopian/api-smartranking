33fe8f6ffcc29a2915737801edde67b10a20a63d
yuriagopian <yuri.agopian@hotmail.com>
2023-02-18 23:53:06 -0300

    chore: exception  filter

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -3,6 +3,7 @@ import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
     throw new Error('Method not implemented.');
   }
 }
