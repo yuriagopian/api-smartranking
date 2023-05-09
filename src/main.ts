079222eab9b771ce47454e624f9a30f927996258
yuriagopian <yuri.agopian@hotmail.com>
2023-05-09 11:12:17 -0300

    chore: add sp time zone

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
+import * as momentTimezone from 'moment-timezone';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,12 +11,24 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // Só sera passado adiante o que estiver maepado
+      whitelist: true, // Só sera passado adiante o que estiver mapeado
       transform: true,
       forbidUnknownValues: true,
-      forbidNonWhitelisted: true, // Se alguma propriedade naõ foi listada e esta sendo passda dispara erro
+      forbidNonWhitelisted: true, // Se alguma propriedade naõ foi listada e esta sendo passada dispara erro
     }),
   );
+  /*
+    Desafio
+    Sobrescrevemos a função toJSON do Date passando um objeto moment. Deste modo 
+    quando o objeto for serializado, ele utilizará o formato de data definido por nós.
+    Todos os objetos Date serão afetados com esta implementação 
+  */
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
+
   await app.listen(3000);
 }
 bootstrap();
